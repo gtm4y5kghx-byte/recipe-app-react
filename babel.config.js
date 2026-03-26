0952101a54750aadb847807b4939cb8fbb301ef4
jasenmp <gtm4y5kghx@privaterelay.appleid.com>
2026-03-26 06:38:47 -0500

    Updating package scripts. Fixing NativeWind issues.

--- babel.config.js
+++ babel.config.js
@@ -2,15 +2,19 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: [
-      ['babel-preset-expo', { jsxImportSource: 'nativewind' }],
+      ["babel-preset-expo", { jsxImportSource: "nativewind" }],
+      "nativewind/babel",
     ],
     plugins: [
-      ['module-resolver', {
-        alias: {
-          '@': './src',
+      [
+        "module-resolver",
+        {
+          alias: {
+            "@": "./src",
+          },
         },
-      }],
-      'react-native-reanimated/plugin',
+      ],
+      "react-native-reanimated/plugin",
     ],
   };
 };
